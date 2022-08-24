6e45bc6ad39e96dbf0de180a76f44801d2ff17a3
Chris Bongers <chrisbongers@gmail.com>
2022-08-24 07:42:56 +0200

    feat: added Vue component

---
+++ src/components/Vue.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      name: 'Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count--">-</button>
+  <pre>{{count}}</pre>
+  <button @click="count++">+</button>
+  <p>I'm a {{name}} component</p>
+</template>
