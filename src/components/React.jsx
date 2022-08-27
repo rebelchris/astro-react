c6ded7b31bee03feecb710fd7722d58f8fe4817a
Chris Bongers <chrisbongers@gmail.com>
2022-08-27 08:12:37 +0200

    feat: added computed store

--- src/components/React.jsx
+++ src/components/React.jsx
@@ -1,9 +1,9 @@
 import { useStore } from '@nanostores/react'
-import {addColor, colors} from "../store/colors";
+import {addColor, colors, primaryColors} from "../store/colors";
 
 export default function React() {
     const colorItems = useStore(colors);
-
+    const primaryItems = useStore(primaryColors);
     return (
         <div>
             <ul>
@@ -13,8 +13,18 @@ export default function React() {
                     </li>
                 ))}
             </ul>
-            <button onClick={() => addColor('blue', '#a0c4ff')}>Change blue</button>
-            <button onClick={() => addColor('red', '#ffadad')}>Add red</button>
+            <button onClick={() => addColor('blue', '#a0c4ff', true)}>Change blue</button>
+            <button onClick={() => addColor('red', '#ffadad', true)}>Add red</button>
+            <button onClick={() => addColor('purple', '#bdb2ff', false)}>Add purple</button>
+            <hr />
+            <h4>Primary colors</h4>
+            <ul>
+                {Object.values(primaryItems).map(({color, hex}) => (
+                    <li key={color} style={{ backgroundColor: hex }}>
+                        {color} {hex}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/store/colors.js
+++ src/store/colors.js
@@ -1,12 +1,20 @@
-import { map } from "nanostores";
+import { computed, map } from "nanostores";
 
 const colors = map({
     blue: {
+        primary: true,
         color: 'blue',
         hex: '#9bf6ff'
+    },
+    green: {
+        primary: false,
+        color: 'green',
+        hex: '#caffbf'
     }
 });
 
-const addColor = (color, hex) => colors.setKey(color, {color, hex});
+const primaryColors = computed(colors, (list) => Object.values(list).filter(item => item.primary))
 
-export { colors, addColor };
+const addColor = (color, hex, primary) => colors.setKey(color, {color, hex, primary});
+
+export { colors, addColor, primaryColors };
